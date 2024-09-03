00a826ab2d239072235436f435ba362fcfb9fd88
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-03 01:41:13 +0000

    >  compile NEMU 3023244369 root Linux 5b54da968661 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:41:13 up 16:01,  0 users,  load average: 0.12, 0.07, 0.01 ca828b74eb876adffcafee7b6efca239cacda667

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -1,5 +1,6 @@
 #include "monitor/watchpoint.h"
 #include "monitor/expr.h"
+#include "nemu.h"
 
 #define NR_WP 32
 
@@ -19,5 +20,48 @@ void init_wp_pool() {
 }
 
 /* TODO: Implement the functionality of watchpoint */
+WP* new_wp(char * exp){
+	assert(free_ != NULL);
+	WP *temp = free_;
+	free_ = free_->next;
+	temp->next = NULL;
+
+	bool success = false;
+	strcpy(temp->exp, exp);
+	temp->value = expr(temp->exp, &success);
+	assert(success);
+
+	if(head==NULL)
+		head = temp;
+	else{
+		WP *p = head;
+		while(p->next)
+			p = p->next;
+		p->next = temp;
+	}
+	return temp;
+}
 
 
+int free_wp(WP *wp) {
+	if(wp == NULL) {
+	    printf("Input something!\n");
+	    return 0;
+	}
+	if(wp == head) {
+	    head = head->next;
+	}
+	else {
+	    WP *p = head;
+	    while(p->next != wp) {
+	        p = p->next;
+	        if(p == NULL) {
+	            return 0; // wp not found
+	        }
+	    }
+	    p->next = wp->next;
+	}
+	wp->next = free_; // Return the wp to the free pool
+	free_ = wp;
+	return 1;
+}
